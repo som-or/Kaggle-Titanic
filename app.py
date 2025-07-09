01e79d8a6c53932d2785044ee2d4cacc0989662a
Somya Gupta <somyagupta191203@gmail.com>
2025-07-09 17:47:40 +0530

    added html file

--- app.py
+++ app.py
@@ -25,6 +25,14 @@ def predict_api():
     print(output)
     return jsonify({ 'prediction': output })
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[request.form.values()]
+    data = pd.DataFrame(data, columns=['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin','Embarked'])
+    prediction = model.predict(data)[0].item()
+    output = 'dead' if prediction == 1 else 'alive'
+    print(output)
+    return render_template('home.html', prediction_text='The Passenger is {}'.format(output))
 
 if __name__== "__main__":
     app.run(debug=True)
